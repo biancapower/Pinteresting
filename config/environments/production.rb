217f58419d756d276f748c44c9c784cbcfa5d9d7
Bianca Power <drbiancampower@gmail.com>
2017-11-15 16:18:15 +1100

    added s3 host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
     s3_region: ENV['AWS_REGION'],
     s3_credentials: {
       bucket: ENV['AWS_BUCKET'],
+      s3_host_name: ENV['AWS_S3_HOST_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
